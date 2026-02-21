2f06a75f623b582a6ea3a64660805a0b8e0e9370
animeshhq <animeshhq@gmail.com>
2026-02-21 15:45:57 +0530

    made a lot of changes

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(
     title="Test API",
     description="Meant for Learning"
 )
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
@@ -19,7 +22,7 @@ posts = [
     },
     {
         "id": 2,
-        "author": "Rohan",
+        "author": "Neo",
         "title": "Asyncio Basics",
         "content": "Asyncio helps run tasks concurrently using async and await.",
         "date_posted": "2026-02-19"
